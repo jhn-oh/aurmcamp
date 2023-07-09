0588b9845e0994f726645e7752513cfbb1d98b7d
Juhyun Oh <119278826+jhn-oh@users.noreply.github.com>
2023-07-09 23:44:04 +0900

    Update aurmcamp.py

--- aurmcamp.py
+++ aurmcamp.py
@@ -14,17 +14,13 @@ def lookup(a, b):
     lookup_return = []
     checklist = []
     matrix[a, b] = 0
-    if 0 <= a <= 4 and 0 <= b-1 <= 4:
-      if matrix[a, b-1] == 1: #왼쪽으로
+    if 0 <= a <= 4 and 0 <= b-1 <= 4 and matrix[a, b-1] == 1: #왼쪽으로
         lookup_return.append([a, b-1])
-    if 0 <= a+1 <= 4 and 0 <= b <= 4:
-      if matrix[a+1, b] == 1: #아래로
+    elif 0 <= a+1 <= 4 and 0 <= b <= 4 and matrix[a+1, b] == 1: #아래로
         lookup_return.append([a+1, b])
-    if 0 <= a-1 <= 4 and 0 <= b <= 4:
-      if matrix[a-1, b] == 1: #위로
+    elif 0 <= a-1 <= 4 and 0 <= b <= 4 and matrix[a-1, b] == 1: #위로
         lookup_return.append([a-1, b])
-    if 0 <= a <= 4 and 0 <= b+1 <= 4:
-      if matrix[a, b+1] == 1: #오른쪽으로
+    elif 0 <= a <= 4 and 0 <= b+1 <= 4 and matrix[a, b+1] == 1: #오른쪽으로
         lookup_return.append([a, b+1])
 
     print(a, b)
@@ -33,10 +29,9 @@ def lookup(a, b):
     if len(lookup_return) == 2: #갈림길
       checkpoint_list = lookup_return
       lookup(lookup_return[0][0], lookup_return[0][1])
-    if len(lookup_return) == 1: #정상
+    elif len(lookup_return) == 1: #정상
       lookup(lookup_return[0][0], lookup_return[0][1])
-    if len(lookup_return) == 0: #막다른 길
-      if len(checkpoint_list) == 2:
+    elif len(lookup_return) == 0 and len(checkpoint_list) == 2: : #막다른 길
         lookup(checkpoint_list[1][0], checkpoint_list[1][1])
         checkpoint_list = []
   
